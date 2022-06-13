ae02f02ea5461bdc3d27d28aa47a03ccb8fd4cc4
Ravindra Rathor <ravindra@dyte.io>
2022-06-13 10:55:53 +0530

    fix(transcript): Fixed some structure issues and userId checks

--- src/middleware/transcript.ts
+++ src/middleware/transcript.ts
@@ -1,11 +1,11 @@
 /**
  * Though this file indicates the integration of Symbl.ai.
- * We are not utilising Symbl.ai messaging in favor of Dyte websocket messaging 
+ * We are not utilising Symbl.ai messaging in favor of Dyte websocket messaging
  */
 
 import axios from 'axios';
 import DyteClient from '@dytesdk/web-core';
-import { RoomMessage } from '@dytesdk/web-core/types/roomNode/RoomNodeClient';
+import { RoomMessage } from '@dytesdk/web-core/types/client/DyteClient';
 
 let ws: WebSocket;
 
@@ -24,6 +24,8 @@ export interface CleanupConfig{
     symblAccessToken: string,
     transcriptionDivId?: string,
 }
+
+const symblIdToPeerIdMap: {[key: string]: string} = {};
 
 // Actual middleware to apply on Dyte
 async function audioTranscriptionMiddleware(audioContext: AudioContext) {
@@ -52,14 +54,16 @@ async function audioTranscriptionMiddleware(audioContext: AudioContext) {
 }
 
 /**
- * 
+ *
  * @param config Required params to initialise the middleware.
  * meeting is needed to send messages across peers and to listen to room messages.
  * symblAccessToken is needed to connect to symbl.ai.
  * noOfTranscriptionsToShow (1 - n) indicates last n transcriptions to show (Default: 3).
- * transcriptionDivId is the id of the div where you want the (noOfTranscriptionsToShow) transcriptions to be populated.
+ * transcriptionDivId is the id of the div where
+ * you want the (noOfTranscriptionsToShow) transcriptions to be populated.
  * If no valid `transcriptionDivId` is provided, we will safely skip dom update.
- * noOfTranscriptionsToCache is the count of transcriptions you want to save in the cache (Default: 200).
+ * noOfTranscriptionsToCache is the count of transcriptions you want
+ *  to save in the cache (Default: 200).
  * @returns the raw result which `meeting.self.addAudioMiddleware` returns
  */
 async function init({
@@ -69,9 +73,8 @@ async function init({
     noOfTranscriptionsToCache = 200,
     transcriptionDivId,
 }: InitConfig) {
-    
     transcriptions.splice(0); // In case, the package gets reinitialized, resetting transcriptions
-    
+
     const uniqueMeetingId = meeting.meta.roomName;
     const symblEndpoint = `wss://api.symbl.ai/v1/streaming/${uniqueMeetingId}?access_token=${symblAccessToken}`;
 
@@ -98,35 +101,42 @@ async function init({
             data.messages?.forEach((message: any) => {
                 // console.log('Live transcript (more accurate): ', message.payload.content, data);
 
-                if (message.from?.id === meeting.self.id) {
+                if (symblIdToPeerIdMap[message.from?.id] === meeting.self.id) {
                     // More accurate Transcript
                     meeting.sendRoomMessage(
                         'audioTranscriptionMessage', // This can be named anything we want
                         {
-                        text: message.payload.content,
-                        isPartialTranscript: false,
-                        startTimeISO: message.duration?.startTime || new Date().toISOString(),
-                        endTimeISO: message.duration?.endTime || new Date().toISOString(),
-                        peerId: meeting.self.id,
-                        displayName: meeting.self.name,
-                    });
+                            text: message.payload.content,
+                            isPartialTranscript: false,
+                            startTimeISO: message.duration?.startTime || new Date().toISOString(),
+                            endTimeISO: message.duration?.endTime || new Date().toISOString(),
+                            peerId: meeting.self.id,
+                            displayName: meeting.self.name,
+                        },
+                    );
                 }
             });
         }
 
         if (data.type === 'message' && Object.prototype.hasOwnProperty.call(data.message, 'punctuated')) {
-            // console.log('Live transcript (less accurate): ', data.message.punctuated.transcript, data);
+            // console.log('Live transcript (less accurate): ',
+            //     data.message.punctuated.transcript, data);
             if (data.message.user?.peerId === meeting.self.id) {
+                // Symbl sends their own user Id in from.id for accurate messages
+                // Need this mapping there to use it to show/send transcripts
+                symblIdToPeerIdMap[data.message.user.id as string] = meeting.self.id;
+
                 meeting.sendRoomMessage(
                     'audioTranscriptionMessage', // This can be named anything we want
                     {
-                    text: data.message.punctuated.transcript,
-                    isPartialTranscript: true,
-                    startTimeISO: data.message.duration?.startTime || new Date().toISOString(),
-                    endTimeISO: data.message.duration?.endTime || new Date().toISOString(),
-                    peerId: meeting.self.id,
-                    displayName: meeting.self.name,
-                });
+                        text: data.message.punctuated.transcript,
+                        isPartialTranscript: true,
+                        startTimeISO: data.message.duration?.startTime || new Date().toISOString(),
+                        endTimeISO: data.message.duration?.endTime || new Date().toISOString(),
+                        peerId: meeting.self.id,
+                        displayName: meeting.self.name,
+                    },
+                );
             }
         }
     };
@@ -188,29 +198,31 @@ async function init({
 
             transcriptions.push(payload);
 
-            // Keep last noOfTranscriptionsToCache transcriptions only to not have a huge array of elements
-            transcriptions = this.transcriptions.slice(noOfTranscriptionsToCache);
-            
-            //Update in DOM
+            // Keeping last noOfTranscriptionsToCache transcriptions only
+            // to not have a huge array of elements
+            transcriptions = transcriptions.slice(-1 * noOfTranscriptionsToCache);
+
+            // Update in DOM
             const transcriptionsDiv = document.getElementById(transcriptionDivId) as HTMLDivElement;
 
-            if(transcriptionsDiv){
+            if (transcriptionsDiv) {
                 transcriptionsDiv.innerHTML = '';
 
                 const transcriptionsToShow = transcriptions.slice(-1 * noOfTranscriptionsToShow);
-                
+
                 transcriptionsToShow.forEach((transcription) => {
                     const speakerSpan = document.createElement('span');
                     speakerSpan.classList.add('dyte-transcription-speaker');
                     speakerSpan.innerText = `${transcription.displayName}: `;
-        
+
                     const transcriptionSpan = document.createElement('span');
                     transcriptionSpan.classList.add('dyte-transcription-text');
                     transcriptionSpan.innerText = transcription.text?.toString();
-        
+
                     const transcriptionLine = document.createElement('span');
+                    transcriptionLine.classList.add('dyte-transcription-line');
                     transcriptionLine.appendChild(speakerSpan).appendChild(transcriptionSpan);
-        
+
                     transcriptionsDiv.appendChild(transcriptionLine);
                 });
             }
@@ -223,25 +235,29 @@ async function init({
 async function cleanup({
     meeting,
     transcriptionDivId,
-}: CleanupConfig){
-    try{
+}: CleanupConfig) {
+    try {
         meeting.self.removeAudioMiddleware(audioTranscriptionMiddleware);
 
-        //Remove from DOM
+        // Remove from DOM
         const transcriptionsDiv = document.getElementById(transcriptionDivId) as HTMLDivElement;
-        if(transcriptionsDiv){
-            transcriptionsDiv.childNodes.forEach(child => {
+        if (transcriptionsDiv) {
+            transcriptionsDiv.childNodes.forEach((child) => {
                 child.remove();
             });
         }
         ws?.close();
-    } catch(ex){
+    } catch (ex) {
         console.error('Failed to close Symbl websocket. Error: ', ex);
     }
+}
+
+function getTranscriptions() {
+    return transcriptions;
 }
 
 export {
     init,
     cleanup,
-    transcriptions,
+    getTranscriptions,
 };
